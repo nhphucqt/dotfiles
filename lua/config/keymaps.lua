9682d87bec07f2078cd797e0a8d1077a7809d49e
nhphucqt <hoangphuc.qti@gmail.com>
2024-05-08 01:17:19 +0700

    feat: add find all/hidden/ignored files in telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,57 +1,76 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- vim.keymaps are automatically loaded on the VeryLazy event
+-- Default vim.keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional vim.keymaps here
 --
-local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 -- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
+vim.keymap.set("n", "+", "<C-a>")
+vim.keymap.set("n", "-", "<C-x>")
 
 -- Delete a word backwards
-keymap.set("n", "dw", 'vb"_d')
+vim.keymap.set("n", "dw", 'vb"_d')
 
 -- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- Disable continuations
-keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
-keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+vim.keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+vim.keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
 
 -- Jumplist
-keymap.set("n", "<C-m>", "<C-i>", opts)
+vim.keymap.set("n", "<C-m>", "<C-i>", opts)
 
 -- New tab
-keymap.set("n", "te", ":tabedit ")
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+vim.keymap.set("n", "te", ":tabedit ")
+vim.keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 -- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
+vim.keymap.set("n", "ss", ":split<Return>", opts)
+vim.keymap.set("n", "sv", ":vsplit<Return>", opts)
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
+vim.keymap.set("n", "sh", "<C-w>h")
+vim.keymap.set("n", "sk", "<C-w>k")
+vim.keymap.set("n", "sj", "<C-w>j")
+vim.keymap.set("n", "sl", "<C-w>l")
 
--- Resize window
-keymap.set("n", "<C-w><left>", "<C-w><")
-keymap.set("n", "<C-w><right>", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
+-- Resize window""
+vim.keymap.set("n", "<C-w><left>", "<C-w><")
+vim.keymap.set("n", "<C-w><right>", "<C-w>>")
+vim.keymap.set("n", "<C-w><up>", "<C-w>+")
+vim.keymap.set("n", "<C-w><down>", "<C-w>-")
 
 -- Diagnostics
-keymap.set("n", "<C-j>", function()
+vim.keymap.set("n", "<C-j>", function()
     vim.diagnostic.goto_next()
 end, opts)
 
--- Copilot
+-- Copilot Plugin
 vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
     expr = true,
     replace_keycodes = false,
 })
 vim.g.copilot_no_tab_map = true
+
+-- Telesope Plugin
+vim.keymap.set(
+    "n",
+    "<Leader>fi",
+    ":lua require('telescope.builtin').find_files{no_ignore=true}<CR>",
+    { desc = "Find files (Root dir) (+I)" }
+)
+vim.keymap.set(
+    "n",
+    "<Leader>fh",
+    ":lua require('telescope.builtin').find_files{hidden=true}<CR>",
+    { desc = "Find files (Root dir) (+H)" }
+)
+vim.keymap.set(
+    "n",
+    "<Leader>fa",
+    ":lua require('telescope.builtin').find_files{no_ignore=true,hidden=true}<CR>",
+    { desc = "Find All files (Root dir)" }
+)
